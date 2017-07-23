98ea1a20ae33e8879224a3d0362fb7bc65723cc8
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-07-23 15:53:50 +0530

    Added question survey bot

--- routes/bot.js
+++ routes/bot.js
@@ -10,9 +10,56 @@ const connector = new builder.ChatConnector({
 
 router.post("/", connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+const userStore = [];
 const bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    // store user's address
+    const address = session.message.address;
+    userStore.push(address);
+
+    // end current dialog
+    session.endDialog('Hey there! We will ask you a few questions to get started...');
 });
+
+// Every 5 seconds, check for new registered users and start a new dialog
+setInterval(function () {
+    const newAddresses = userStore.splice(0);
+    newAddresses.forEach(function (address) {
+
+        console.log('Starting survey', address);
+
+        // new conversation address, copy without conversationId
+        const newConversationAddress = Object.assign({}, address);
+        delete newConversationAddress.conversation;
+
+        // start survey dialog
+        bot.beginDialog(newConversationAddress, 'survey', null, function (err) {
+            if (err) {
+                // error ocurred while starting new conversation. Channel not supported?
+                bot.send(new builder.Message()
+                    .text('This channel does not support this operation: ' + err.message)
+                    .address(address));
+            }
+        });
+
+    });
+}, 60000);
+
+bot.dialog('survey', [
+    function (session) {
+        builder.Prompts.text(session, 'Hello... What\'s your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.choice(session,
+            'Hi ' + results.response + ', for which Resume Manager do you want updates? ',
+            ['Internship', 'Placement']);
+    },
+    function (session, results) {
+        session.userData.updateType = results.response.entity;
+        session.endDialog('Got it... ' + session.userData.name +
+            ' you want updates for ' + session.userData.coding +
+            ' Resume Manager.');
+    }
+]);
 
 module.exports = router;
