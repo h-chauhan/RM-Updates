6de5898755e4731ea92973de09902630a30413f4
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-07-23 17:48:37 +0530

    Firebase to store subscribed users

--- routes/bot.js
+++ routes/bot.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const builder = require('botbuilder');
+var firebase = require('firebase');
+firebase.initializeApp(require('../config/firebaseConfig'));
+var database = firebase.database();
+var userRef = database.ref('users/');
+
 
 // Create chat connector for communicating with the Bot Framework Service
 const connector = new builder.ChatConnector({
@@ -10,11 +15,10 @@ const connector = new builder.ChatConnector({
 
 router.post("/", connector.listen());
 
-const userStore = [];
 const bot = new builder.UniversalBot(connector, function (session) {
     // store user's address
-    userStore.push({
-        id: session.message.user.id,
+    var newUser = userRef.child(session.message.user.id);
+    newUser.set({
         address: session.message.address
     });
 
@@ -23,40 +27,37 @@ const bot = new builder.UniversalBot(connector, function (session) {
 });
 
 // Every 5 seconds, check for new registered users and start a new dialog
-setInterval(function () {
-    const newIds = userStore.splice(0);
-    newIds.forEach(function (obj) {
+userRef.limitToLast(1).on('child_added', function (snapshot) {
 
-        console.log('Starting survey', obj.id);
+    // all records after the last continue to invoke this function
+    var newUserID = snapshot.key;
+    console.log('Starting survey', newUserID);
 
-        // new conversation address, copy without conversationId
-        const newConversationAddress = Object.assign({}, obj.address);
+    const newConversationAddress = Object.assign({}, snapshot.val().address);
 
-        // start survey dialog
-        bot.beginDialog(newConversationAddress, 'survey', null, function (err) {
-            if (err) {
-                // error ocurred while starting new conversation. Channel not supported?
-                bot.send(new builder.Message()
-                    .text('This channel does not support this operation: ' + err.message)
-                    .address(address));
-            }
-        });
-
+    // start survey dialog
+    bot.beginDialog(newConversationAddress, 'survey', null, function (err) {
+        if (err) {
+            // error ocurred while starting new conversation. Channel not supported?
+            bot.send(new builder.Message()
+                .text('This channel does not support this operation: ' + err.message)
+                .address(address));
+        }
     });
-}, 5000);
+});
 
 bot.dialog('survey', [
     function (session) {
         builder.Prompts.text(session, 'Hello... What\'s your name?');
     },
     function (session, results) {
-        session.userData.name = results.response;
+        userRef.child(session.message.user.id).child("name").set(results.response);
         builder.Prompts.choice(session,
             'Hi ' + results.response + ', for which Resume Manager do you want updates? ',
             ['Internship', 'Placement']);
     },
     function (session, results) {
-        session.userData.updateType = results.response.entity;
+        userRef.child(session.message.user.id).child("updateType").set(results.response.entity);
         session.endDialog('Got it... ' + session.userData.name +
             ' you want updates for ' + session.userData.updateType +
             ' Resume Manager.');
