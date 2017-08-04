03808f8b4f648c74e5f4dd4c5773c0e72e4be529
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-08-04 13:47:15 +0530

    Login

--- config/rmConfig.js
+++ config/rmConfig.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    loginUrl: "http://tnp.dtu.ac.in/rm_2016-17/login/",
+    loginUrl: "http://tnp.dtu.ac.in/rm_2016-17/intern/intern_login",
+    newsFeedUrl: "http://tnp.dtu.ac.in/rm_2016-17/intern/intern_student",
     username: process.env.RM_LOGIN_USERNAME,
     password: process.env.RM_LOGIN_PASSWORD
 };
