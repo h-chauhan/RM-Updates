da1b586750d7b31dd0c902cc1036de521648786f
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-08-12 15:53:02 +0530

    job openings

--- config/rmConfig.js
+++ config/rmConfig.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-    loginUrl: "http://tnp.dtu.ac.in/rm_2016-17/login",
-    newsFeedUrl: "http://tnp.dtu.ac.in/rm_2016-17/student",
+    loginUrl: "http://tnp.dtu.ac.in/rm_2016-17/login/",
+    newsFeedUrl: "http://tnp.dtu.ac.in/rm_2016-17/student/",
+    jobsUrl: "http://tnp.dtu.ac.in/rm_2016-17/student/job_openings/",
     username: process.env.RM_LOGIN_USERNAME,
     password: process.env.RM_LOGIN_PASSWORD,
     internLoginUrl: "http://tnp.dtu.ac.in/rm_2016-17/intern/intern_login",
