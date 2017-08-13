9529b1cc7d54993177c98a04776df75acd6c74ab
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-08-13 17:33:49 +0530

    Fix cron jobs

--- config/cronConfig.js
+++ config/cronConfig.js
@@ -2,6 +2,6 @@ module.exports = {
 	// CRON fields : sec min hour day month day
 	// TODO update them to right values
 	pullRMSchedule: "0 */1 * * * *",
-
-	pingMyselfSchedule: "0 */15 * * * *",
+	pullRMJobsSchedule: "0 */5 * * * *",
+	pingMyselfSchedule: "0 */5 * * * *"
 };
