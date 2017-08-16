bf55d0a643b8fd777881f38eb193ff13a064b5bd
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-08-16 08:55:41 +0530

    stupid me

--- config/cronConfig.js
+++ config/cronConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
 	// CRON fields : sec min hour day month day
 	// TODO update them to right values
-	pullRMSchedule: "0 */1 7-17 * * *",
-	pullRMJobsSchedule: "0 */5 7-17 * * *",
+	pullRMSchedule: "0 */1 2-17 * * *",
+	pullRMJobsSchedule: "0 */5 2-17 * * *",
 	pingMyselfSchedule: "0 */15 * * * *"
 };
