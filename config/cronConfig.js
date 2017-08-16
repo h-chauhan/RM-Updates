7cb0377a8bda13d50106e87fa69254087ec154d4
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-08-16 08:20:15 +0530

    jobs using new api

--- config/cronConfig.js
+++ config/cronConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
 	// CRON fields : sec min hour day month day
 	// TODO update them to right values
-	pullRMSchedule: "*/10 * 7-23 * * *",
-	pullRMJobsSchedule: "0 */5 8-23 * * *",
+	pullRMSchedule: "*/10 * 8-23 * * *",
+	pullRMJobsSchedule: "*/10 * 8-23 * * *",
 	pingMyselfSchedule: "0 */15 * * * *"
 };
