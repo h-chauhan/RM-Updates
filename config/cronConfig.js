669c538e3360bdb60c80cc053f26f1a5fd65884f
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-08-16 07:36:32 +0530

    using new proxy server

--- config/cronConfig.js
+++ config/cronConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
 	// CRON fields : sec min hour day month day
 	// TODO update them to right values
-	pullRMSchedule: "0 */2 * * * *",
-	pullRMJobsSchedule: "0 */5 * * * *",
-	pingMyselfSchedule: "0 */5 * * * *"
+	pullRMSchedule: "*/10 * 7-23 * * *",
+	pullRMJobsSchedule: "0 */5 8-23 * * *",
+	pingMyselfSchedule: "0 */15 * * * *"
 };
