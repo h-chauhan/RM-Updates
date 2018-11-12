cd5e6b35a596da58c02ae25d4dd08ae3b1cbdfcb
Himanshu Chauhan <hchauhan1404@outlook.com>
2018-11-12 19:58:43 +0530

    Update service.config.js

--- configs/service.config.js
+++ configs/service.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    PLACEMENT_NOTIFS_URL: "http://rmbot-service.herokuapp.com/api/notifs/",
-    PLACEMENT_JOBS_URL: "http://rmbot-service.herokuapp.com/api/jobs/",
-    INTERNSHIP_NOTIFS_URL: "http://rmbot-service.herokuapp.com/api/internnotifs/",
-    INTERNSHIP_JOBS_URL: "http://rmbot-service.herokuapp.com/api/internjobs/",
+    PLACEMENT_NOTIFS_URL: "http://142.93.209.188/api/placement/notifications/",
+    PLACEMENT_JOBS_URL: "http://142.93.209.188/api/placement/jobs/",
+    INTERNSHIP_NOTIFS_URL: "http://142.93.209.188/api/internship/notifications/",
+    INTERNSHIP_JOBS_URL: "http://142.93.209.188/api/internship/jobs/",
 };
